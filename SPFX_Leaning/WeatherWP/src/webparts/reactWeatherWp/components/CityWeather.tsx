b6d87faab571c6b5f415feeb1abe861cfa42a462
Milan <parikh.milan87@gmail.com>
2020-05-06 17:08:24 -0400

    Commit changes

--- SPFX_Leaning/WeatherWP/src/webparts/reactWeatherWp/components/CityWeather.tsx
+++ SPFX_Leaning/WeatherWP/src/webparts/reactWeatherWp/components/CityWeather.tsx
@@ -1,3 +1,12 @@
-import React from 'react';
+import React ,{FunctionComponent} from 'react';
 
-export default CityWeather : React
+export default CityWeather : FunctionComponent<any> = (properties: any) => {
+    return `
+     <div>
+      <span class="${ styles.title }"> ${ city.name }</span>
+      <span class="${ styles.title }"> ${ city.temp }</span>
+      <span class="${ styles.title }"> ${ city.humidity }</span>
+      <span class="${ styles.title }"> ${ city.climate }</span>
+     </div>
+    `
+}
