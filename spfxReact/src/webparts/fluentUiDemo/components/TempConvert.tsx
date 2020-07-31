16cfb18eba01b05010798946d0305192b0c0b23a
Milan Parikh <milan.parikh@dart.biz>
2020-07-31 06:31:48 -0400

    Fluent UI Control Demo

---
+++ spfxReact/src/webparts/fluentUiDemo/components/TempConvert.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import styles from './FluentUiDemo.module.scss';
+
+import { Label, TextField, PrimaryButton, } from "office-ui-fabric-react";
+
+export function TempConvert(props: any) : JSX.Element {
+    let [celcius,setCelcius] = React.useState(0);
+    let [faren, setFaren] = React.useState(0);
+
+    return (
+      
+            <div>
+              <span className={ styles.title }>Temp Converter!</span>
+              <div>
+                <TextField label="Celcius : " onChange={ (event, value) => {
+                  setCelcius(parseFloat(value));
+                }} /><br/>
+                
+
+                <PrimaryButton text=" Convert " onClick={ () => {
+                    let f = ((9/5)* celcius) +32.0;
+
+                    setFaren(f);
+
+                }}/><br/><br/>
+
+                <Label>Farenheit : { faren }</Label>
+              </div>
+              
+            </div>
+
+    );
+}
