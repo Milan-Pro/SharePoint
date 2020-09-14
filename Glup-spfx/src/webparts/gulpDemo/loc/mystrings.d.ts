e5fe5e6a555444ae90546f65764ed3d9a20f9351
Milan Parikh <milan.parikh@dart.biz>
2020-09-14 06:52:12 -0400

    gulp basic

---
+++ Glup-spfx/src/webparts/gulpDemo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IGulpDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'GulpDemoWebPartStrings' {
+  const strings: IGulpDemoWebPartStrings;
+  export = strings;
+}
