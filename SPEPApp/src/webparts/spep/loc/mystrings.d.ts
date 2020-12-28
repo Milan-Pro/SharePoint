991a49d69a949d759097be27120623c803f28953
Milan <parikh.milan87@gmail.com>
2020-12-28 15:14:14 -0500

    spep initial

---
+++ SPEPApp/src/webparts/spep/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpepWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpepWebPartStrings' {
+  const strings: ISpepWebPartStrings;
+  export = strings;
+}
